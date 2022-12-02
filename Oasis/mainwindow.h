63583ded9ce0eaea94b7bf1a0ee616fae09dd1e6
Andre-Nonaka <andrenonakacordova@cmail.carleton.ca>
2022-12-02 18:41:58 -0500

    Adding more comments

--- Oasis/mainwindow.h
+++ Oasis/mainwindow.h
@@ -144,29 +144,77 @@ private slots:
     // the respective session number in the intensity column (the column that goes from 1 to 8 in the GUI). It does
     // this for 3 seconds.
     void flashTimer();
+
+    // This slot is called when the QTimer named timerCes timeout. It calls the toggleCesModeLight() function to turn on
+    // or off the CES mode light for 3 seconds (3000 miliseconds) at an interval of 500 miliseconds.
     void flashGraphCounter();
+
+    // This slot is called when the QTimer named pauseTimerDefault timeout. It calls the intervalTimerIntensity() that performs
+    // the scrolling animation (when the device's disconnected). It does this for 8 seconds (8000 miliseconds).
     void pauseCounter();
+
+    // This slot is called when the QTimer named timerBlinkLed timeout. It checks what group of LEDs will be called based
+    // on the connection (connected or disconnected) and state (dry or wet).
+    // - If the state is dry and the device's connected, the LEDs 4, 5, and 6 will blink yellow (turn on and off) at the same time.
+    //   This means that the user has an okay connection.
+    // - If the state is wet and the device's connected, the LEDs 1, 2, and 3 will blink green (turn on or off) at the same time.
+    //   This means that the user has a good connection.
+    // - If the state is either dry or wet and the device's disconnected, the LEDs 7 and 8 will blink red (turn on or off) at the same time.
+    //   This means that the user has no connection.
+    // It will blink these group of LEDs for 3 seconds (3000 miliseconds) at an interval of 500 miliseconds.
     void blinkCounter();
+
+    // This slot is called when the QTimer named intensityTimer timeout. This slot performs the turn on and off the LEDs (the labels
+    // 1 to 8 found in the UI) in scrolling manner (turns on and off one at a time) in an interval of 500 miliseconds.
+    // Once it reaches 15 (as there is 15 times that it needs to turn on or off of the LEDs - 8 numbers * 2 states (turn on or off) equals
+    // to 16 (15 goes from 0 to 15, which is 16 as it counts 0 as well), it stops the timer and reset the countSwitch counter back to 0.
+    // The countSwitch keeps track of how many times the slot has been called (when it reaches 15 the animation ends).
     void switchLeds();
+
+    // This slot is called the user presses the new battery button. This will make the battery of the device go back
+    // to 100%.
     void on_newBattery_clicked();
+
+    // This slot is called when the user changes the connectivity of the application (switches from connected to
+    // disconnected and vice-versa). This checks if the device is fully turned on and that a session has started in
+    // order for the change to go through (this means once it has confirmed that the device is fully on and that a
+    // session has started, it will proceed to restart the connection test).
     void on_listOfSkins_currentIndexChanged(const QString &arg1);
+
+    // This slot is called when the QTimer named fiveSecondsDelay timeout. As a SingleShot QTimer, we do not need
+    // to stop the fiveSecondsDelay QTimer, which means that this slot just calls the connection test function to
+    // start the connection test.
     void continueAfter5Seconds();
+
+    // This slot is called when the user switches from wet to dry and vice-versa.
+    // This checks if the device is fully turned on and that a session has started in
+    // order for the change to go through (this means once it has confirmed that the device is fully on and that a
+    // session has started, it will proceed to restart the connection test).
     void on_listWetOrDry_currentIndexChanged(const QString &arg1);
+
+    // This slot is called when the QTimer named endSession timeout. It will start the descend (scrolling down) animation
+    // in the intensity section of UI (where the vertical labels containing number from 1 to 8 are found).
     void startDescendEndSession();
 
 private:
     Ui::MainWindow *ui;
     Database *newDatabase = new Database();
     QVector<int> levels;
+
     int numberOfTimesPowerBtnClicked = 0;
+
+    // Duration and session variables.
     int newRowItemDuration = 0;
     int newRowItemSession = 0;
     int selectedDuration = 0;
     int selectedSession = 0;
+
     QTimer *batteryStartTimer = nullptr;
     QTimer *batteryStopTimer = nullptr;
     bool selectedSessionOrNot = false;
     int batteryLevel = 43;
+
+    // LEDs variables (boolean)
     bool ledOneStatus = false;
     bool ledTwoStatus = false;
     bool ledThreeStatus = false;
@@ -180,12 +228,17 @@ private:
     bool ledFiveFlash = false;
     bool ledSixFlash = false;
     bool ledSevenFlash = false;
+
+    // Timer variable to check if user does
+    // not click selection button after 2 minutes.
     QTimer *timer = nullptr;
+
     // Connection test:
     bool graphSessionStatus = false;
     bool connectivity = true;
     QTimer *timerFlashes = nullptr;
     QTimer *timerCES = nullptr;
+
     // Connection test:
     /* CES Mode light control */
     QTimer *connectionTestStartTimer = nullptr;
@@ -197,12 +250,14 @@ private:
     int valuePause = 0;
     int countForPauseEnd = 0;
     QTimer *timerBlinkLed = nullptr;
+
     /*
      * Grp 0 ====> blinking LEDs 7 and 8
      * Grp 1 ====> blinking LEDs 4, 5, and 6
      * Grp 2 ====> blinking LEDs 1, 2, and 3
      */
     int groupToBlink = 0;
+
     int counterBlinkingLed = 0;
     bool blinkTrueOrFalse = true;
     int signal = 0;
